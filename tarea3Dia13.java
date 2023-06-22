56e333d3e4a050a2ddbffee8d531fcb603e0b7ce
tomasgrasetti2021 <136123948+tomasgrasetti2021@users.noreply.github.com>
2023-06-21 18:19:23 -0600

    Add files via upload

---
+++ tarea3Dia13.java
@@ -0,0 +1,19 @@
+
+public class tarea3Dia13 {
+	
+	
+	public static void main(String[] args) {
+		
+		 int numero1 = 17;
+		 int numero2 = 3;
+		
+		int cociente = numero1 / numero2;
+		System.out.println("El cociente es: " + cociente);
+		int resto = numero1 % numero2;
+		System.out.println("El resto es: " + resto);
+		double respuestaReal = (double) numero1 / numero2;
+		System.out.println("La respuesta real es: " + respuestaReal);
+		
+	}
+
+}

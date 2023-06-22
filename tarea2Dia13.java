0e62136e9d2d51af8168aae490ef7d112b6b112f
tomasgrasetti2021 <136123948+tomasgrasetti2021@users.noreply.github.com>
2023-06-21 18:33:13 -0600

    Add files via upload

---
+++ tarea2Dia13.java
@@ -0,0 +1,13 @@
+
+public class tarea2Dia13 {
+	public static void main(String[] args) {
+		
+		String significados = "Significados1: " + (-16 + 32 / 4)+
+				"Resultado 2: " + ((48 - 7) % 11) +
+				"Resultado 3: " + ((18 + 2) / (17 - 12)) +
+				"Resultado 4: " + (17 - 18 / 6 * 3 + 11 % 4);
+		
+		System.out.println(significados);
+	
+}
+}
